bb33f0f83c4f5decb06bfaf99e08ed27d75bd222
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2022-05-07 12:04:46 +0200

    Update welcome.blade.php

--- resources/views/launched/welcome.blade.php
+++ resources/views/launched/welcome.blade.php
@@ -15,6 +15,6 @@ $quote = Inspiring::quote();
     </x-eduka::head>
 
     <x-eduka::body class="antialiased bg-body text-body font-body">
-        okay
+        <a href="#">@paylink('url', ['ip' => '123.33.12.23'], ['key' => 'okay'])</a>
     </x-eduka::body>
 </x-eduka::site>
